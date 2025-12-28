6ff525f8f156315be31dfc1ccef184b74f2a1d77
ToryaToria <toria.torya@yandex.ru>
2025-12-28 11:12:48 +0300

    подготовка проекта для портфолио

--- build/scripts/mobil-menu.js
+++ build/scripts/mobil-menu.js
@@ -1,9 +1,1 @@
-// source/scripts/mobil-menu.js
-var btnMob = document.querySelector(".js-toggle-button");
-var menuMob = document.querySelector(".nav__list");
-var onClickBtn = () => {
-  btnMob.classList.toggle("js-toggle-button--closer");
-  menuMob.classList.toggle("hidden");
-};
-btnMob.addEventListener("click", onClickBtn);
-//# sourceMappingURL=mobil-menu.js.map
+var t=document.querySelector(".js-toggle-button"),e=document.querySelector(".nav__list"),o=()=>{t.classList.toggle("js-toggle-button--closer"),e.classList.toggle("hidden")};t.addEventListener("click",o);
